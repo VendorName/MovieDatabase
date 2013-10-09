d08ca592d4032819f81389ead25b30c5f525feeb
Mihály Farkas <mihaly.farkas@megaweb.hu>
2013-10-09 13:18:22 +0200

    hungarian translation

---
+++ languages/movieDatabase/hu.php
@@ -0,0 +1,15 @@
+<?php
+ 
+return array(
+	'movieDatabase.field.title.label' => 'Cím',
+    'movieDatabase.field.releaseYear.label' => 'Bemutatás éve',
+    
+    'movieDatabase.admin.menu.movieDatabaseList' => 'Filmek',
+    'movieDatabase.field.imdbId.label' => 'Imdb Id',
+    'movieDatabase.field.actions.label' => 'Műveletek',
+    
+    'movieDatabase.message.save.success' => 'Film sikeresen mentve.',
+    'movieDatabase.message.save.failed' => 'Film mentése sikertelen.',
+    
+    'movieDatabase.field.plot.label' => 'Tartalom'
+);
